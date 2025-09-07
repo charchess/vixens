297db6bf302aab800a488c6e7420c7d06bba8b18
Charchess <charchess@gmail.com>
2025-09-07 11:33:14 +0200

    global : correction des config pour dev et validation sans les mains (pas de patch ou autre, juste du gitops pur)

---
+++ clusters/vixens/argocd/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ENV="$1"
+
+if [[ -z "$ENV" ]]; then
+  echo "Usage: $0 <dev|prod>"
+  exit 1
+fi
+
+if [[ "$ENV" != "dev" && "$ENV" != "prod" ]]; then
+  echo "Erreur : l'argument doit être 'dev' ou 'prod'"
+  exit 1
+fi
+
+export KUBECONFIG=~/vixens/kubeconfig-"$ENV"
+
+helm upgrade --install argocd argo/argo-cd --version 8.3.1 --namespace argocd --create-namespace
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.1.1/manifests/crds/appproject-crd.yaml
+
+kubectl apply -f clusters/vixens/argocd/00-vixens-project.yaml
+kubectl apply -f clusters/vixens/argocd/01-vixens-$ENV-root.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
