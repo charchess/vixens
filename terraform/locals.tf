c3d24f168c97fe29b07b7bfd073d14493bb5a856
Charchess <charchess@gmail.com>
2025-09-12 02:24:34 +0200

    Remove exposed secret

--- terraform/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  machine_patches = {
-  for k in keys(var.machines) :
-    k => file("path.module/vixens−dev−{k}.yaml")
-  }
-}
--- terraform/talos_config.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  # On fusionne controlplane.yaml + patch machine
-  machine_configs = {
-    for k, v in var.machines :
-    k => base64encode(templatefile("${path.module}/vixens-dev-${k}.yaml", {}))
-  }
-}
