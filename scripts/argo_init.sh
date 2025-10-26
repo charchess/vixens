fa0f7aab55eaed13d6ce9c51232e6b4777abdb95
Charchess <charchess@gmail.com>
2025-10-26 17:07:51 +0100

    v0.0.0 - fresh restart

--- scripts/argo_init.sh
+++
@@ -1,2 +0,0 @@
-ARGOPASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-argocd login argocd.truxonline.com --username admin --password $ARGOPASSWORD
