4a31322725ec20941bd74d280e6b65b16fea5ea2
Charchess <charchess@gmail.com>
2025-10-28 14:05:36 +0100

    v2.0.0 - on reprends tout a zero (si si)

--- terraform/environments/dev/main.tf
+++
@@ -1,8 +0,0 @@
-# Call the reusable talos-cluster module
-module "vixens_dev_cluster" {
-  source = "../../modules/talos-cluster"
-
-  cluster_name          = var.cluster_name
-  cluster_endpoint      = var.cluster_endpoint
-  control_plane_nodes = var.control_plane_nodes
-}
